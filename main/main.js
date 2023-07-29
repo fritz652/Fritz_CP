c6fc3cb4fb0ed06f6a1abb85985a8da99ecca79c
Fritz <fritz.developer10@gmail.com>
2023-07-28 23:22:29 -0500

    ok

--- main/main.js
+++ main/main.js
@@ -1,7 +1,17 @@
-let btnPrimary = document.getElementById("btnPrimary");
+let btnPrimary = document.querySelector("#btnPrimary");
 btnPrimary.addEventListener("click", function() {
-    alert("Haz hecho clic ?")
+    alert("Haz hecho clic aquí?")
 });
+
+console.log(btnPrimary);
+
+
+let slogan = document.querySelector("#frase");
+
+slogan.innerHTML = "La solución mas libre en credito e <br> inversión";
+
+console.log(slogan.innerHTML);
+
 
 
 
